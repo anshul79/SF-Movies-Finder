2835b8257c645b81f20ef374d44baf6134774a0d
Anshul Jain <anshul.jain@gwynniebee.com>
2020-08-24 03:30:57 +0530

    fetching unique locations

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,6 @@ import { render } from "react-dom";
 import SearchMovies from "./SearchMovies";
 
 class App extends React.Component {
-  componentDidMount() {
-    fetch("https://data.sfgov.org/resource/yitu-d5am.json")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
-  }
-
   render() {
     return (
       <div>
--- src/SearchMovies.jsx
+++ src/SearchMovies.jsx
@@ -1,25 +1,85 @@
 import React, { useState } from "react";
 
-const SearchParams = () => {
-  const [location, setLocation] = useState("San Francisco");
+class SearchParams extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      locations: []
+    };
+  }
 
-  return (
-    <div className="serach-params">
-      <h1>{location}</h1>
-      <form>
-        <label htmlFor="location">
-          Enter your Location:
-          <input
-            id="location"
-            value={location}
-            placeholder="Location"
-            onChange={(event) => setLocation(event.target.value)}
-          />
-        </label>
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+  fetchUniqueMovies(jsonResponse) {
+    let arr = [];
+    let mySet = new Set();
+
+    jsonResponse.map(item => {
+      if (item.locations) {
+        mySet.add(item.locations);
+      }
+    });
+    arr = Array.from(mySet);
+    
+    return arr;
+  }
+
+  componentDidMount() {
+    fetch("https://data.sfgov.org/resource/yitu-d5am.json")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          var uniqueLocations = this.fetchUniqueMovies(result);
+          this.setState({
+            isLoaded: true,
+            items: result,
+            locations: uniqueLocations
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items, locations } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="serach-params">
+          <form>
+            <label htmlFor="location">
+              Enter your Location:
+              <input
+                id="location"
+                type="text"
+                name="mylocation"
+              />
+            </label>
+            <button id="movie-submit">Submit</button>
+          </form>
+          <br />
+          <br />
+          All locations:
+          <ul>
+            {locations.map(item => (
+              <li key={item}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
 };
 
 export default SearchParams;
